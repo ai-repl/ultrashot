2d6ec60938fa91759bd18f66a62a54ddc65288a7
Robert Shaw <xiaoluoboding@gmail.com>
2024-10-20 20:17:32 +0800

    feat: add rate limit

--- app/api/imagine/route.ts
+++ app/api/imagine/route.ts
@@ -1,12 +1,32 @@
 import { NextResponse, type NextRequest } from "next/server";
 import Replicate from "replicate";
+import { Ratelimit } from "@upstash/ratelimit";
+
+import { redis } from "@/lib/redis";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_KEY,
   useFileOutput: false,
 });
 
+const ratelimit = redis
+  ? new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.slidingWindow(5, "1440 m"),
+      analytics: true,
+    })
+  : false;
+
 export async function POST(req: NextRequest) {
+  if (process.env.NODE_ENV === "production" && ratelimit) {
+    const ip = req.headers.get("x-real-ip") ?? "local";
+    const rl = await ratelimit.limit(ip);
+
+    if (!rl.success) {
+      return new Response("Rate limit exceeded", { status: 429 });
+    }
+  }
+
   if (!process.env.REPLICATE_API_KEY || process.env.REPLICATE_API_KEY === "") {
     return new Response(
       "Missing REPLICATE_API_KEY – make sure to add it to your .env file.",
